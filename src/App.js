6a148dbcc1514e25a7e1caa0b83f929c74f03171
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-09 15:23:06 -0500

    Form component set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
+// importing components
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-       <header>
-      <h1>My Todo List</h1>
-    </header>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
