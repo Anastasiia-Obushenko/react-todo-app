f4bd51eec66f71699cea2415e452d880d3953b42
Anastasiia Obushenko <n.obushenko@gmail.com>
2023-01-09 21:20:38 -0500

    subtimiting todos

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [inputText, setinputText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>My Todo List</h1>
       </header>
-      <Form setinputText={setinputText} />
+      <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} />
       <TodoList />
     </div>
   );
